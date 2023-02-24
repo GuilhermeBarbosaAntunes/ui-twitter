72d3de4d767064db7cdb78e6a39e763f64bcc15e
Guilherme Barbosa <guilhermebarbosaantunes@gmail.com>
2023-02-24 15:14:48 -0300

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { Sparkle } from "phosphor-react";
+import './Header.css'
+export default function Header() {
+  return (
+    <div className='timeline-header'>
+            Home
+            <Sparkle />
+          </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './components/global.css'
+import './global.css';
+
+import Tweet from './components/Tweet';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-   <div className='layout'>
-    <aside className='sidebar'></aside>
-    <div className='content'>
-      
+    <div className='layout'>
+       <Sidebar /> 
+      <div className='content'>
+        <main className='timeline'>
+          <Header />
+
+          <form className='new-tweet-form'>
+            <label htmlFor="tweet">
+              <img src="https://github.com/GuilhermeBarbosaAntunes.png" alt="Guilherme Barbosa" />
+              <textarea id='tweet' placeholder="What's happening?" />
+            </label>
+
+            <button type='submit'>Tweet</button>
+          </form>
+          <div className="separator"></div>
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+        </main>
+      </div>
     </div>
-   </div>
   </React.StrictMode>,
 )
